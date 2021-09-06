0436a36dece7fedf6aa2e505aa8b9aa5e44cf75a
Emil Christiansen <emilsen68@gmail.com>
2021-09-06 23:26:54 +0200

    Update lightController.py

--- PiCode/lightController.py
+++ PiCode/lightController.py
@@ -185,7 +185,7 @@ def ballDown1(strip, origin):
                     stripBrightness[i]["up"] = False
 
                     if i < len(stripBrightness) - 1 and animationComplete == False and stripBrightness[i]["forwards"]:
-                        print(i, strip.numPixels())
+                        print(i, strip.numPixels() - 2)
                         if i >= strip.numPixels() - 1:
                             stripBrightness[0]["active"] = True
                         else:
