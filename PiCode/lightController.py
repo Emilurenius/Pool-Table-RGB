e5be64b94f58cc712866f11c6adfa5e4be159d0f
Emil Christiansen <emilsen68@gmail.com>
2021-09-06 23:45:15 +0200

    Update lightController.py

--- PiCode/lightController.py
+++ PiCode/lightController.py
@@ -235,7 +235,7 @@ if __name__ == '__main__':
     while True:
         ballsDown = requests.get(f"{serverAddress}/getBallsDown").json()
         if "1" in ballsDown:
-            ballDown1(strip, 5)
+            ballDown1(strip, 90)
         time.sleep(0.1)
 
     
