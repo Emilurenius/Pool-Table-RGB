b0860ea223c65742aace5a43e6cb63b832f948eb
Emil Christiansen <emilsen68@gmail.com>
2021-09-12 12:47:41 +0200

    Update webCamCap.py

--- imageRecognition/webCamCap.py
+++ imageRecognition/webCamCap.py
@@ -1,5 +1,4 @@
 import cv2, numpy as np
-kernel = np.ones((5,5),np.uint8)
 
 print("Imported module")
 cap = cv2.VideoCapture(1)
@@ -15,11 +14,6 @@ while True:
     success, img = cap.read()
     print(success)
     if success:
-        imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
-        imgCanny = cv2.Canny(img,150,200)
-        imgDialation = cv2.dilate(imgCanny,kernel,iterations=1)
-        imgEroded = cv2.erode(imgDialation,kernel,iterations=1)
-        cv2.imshow("Video",imgEroded)
+        cv2.imshow("Video",img)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
