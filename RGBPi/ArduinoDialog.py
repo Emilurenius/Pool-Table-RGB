aaef4afa5a6b638ea47bccba3d87c888afc3ed58
Emil Christiansen <emilsen68@gmail.com>
2021-09-24 22:42:15 +0200

    Update ArduinoDialog.py

--- RGBPi/ArduinoDialog.py
+++ RGBPi/ArduinoDialog.py
@@ -1,4 +1,4 @@
-import time, RPi.GPIO as GPIO
+import time, RPi.GPIO as GPIO, requests, random
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(16, GPIO.IN)
@@ -11,3 +11,7 @@ while True:
     if (GPIO.input(16)):
         print("Ball detected!")
         GPIO.output(12, True)
+        
+        hole = random.randint(0, 5)
+        requests.get(f"http://172.16.4.195:3000/balldown?hole={hole}")
+        print(hole, "Activated")
