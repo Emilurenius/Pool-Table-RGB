d5e34152c12f660c65d5673e36c7ba570cd66609
Emil Christiansen <emilsen68@gmail.com>
2021-09-26 11:01:59 +0200

    Update randomHoleActivations.py

--- externalControl/randomHoleActivations.py
+++ externalControl/randomHoleActivations.py
@@ -1,7 +1,7 @@
 import time, requests, random
 
 while True:
-    time.sleep(random.randint(1, 5))
+    time.sleep(0.1)
     hole = random.randint(0, 5)
     requests.get(f"http://172.16.4.195:3000/balldown?hole={hole}")
     print(hole, "Activated")
